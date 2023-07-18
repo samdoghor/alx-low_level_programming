7d2beafa31336837549c78602ea50505545b8740
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-07-17 18:58:11 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,31 @@
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    6-size.c                                    /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: Samuel Doghor <samdoghor@gmail.com>   /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*    Created: 2023-07-18 02:59:01 by SamDoghor         \/        \/      \_/ *
+*    Updated: 2023-07-18 02:59:01 by SamDoghor                               *
+*                                                                            *
+******************************************************************************
+*/
+
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	system("rm barhamounafissa > sorti 2>&1");
+	exit(1);
+}
