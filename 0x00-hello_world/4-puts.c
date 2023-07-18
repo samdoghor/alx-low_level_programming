db21b97e4201ba79c11a60f85a3b465dcf47b59c
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-07-17 18:50:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    4-puts.c                                    /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: Samuel Doghor <samdoghor@gmail.com>   /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*    Created: 2023-07-18 02:50:43 by SamDoghor         \/        \/      \_/ *
+*    Updated: 2023-07-19 05:50:43 by SamDoghor                               *
+*                                                                            *
+******************************************************************************
+*/
+
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
