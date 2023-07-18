57fd70238cd92359db5daa9b670abd8856038533
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-07-17 18:52:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,26 @@
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    5-printf.c                                  /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: Samuel Doghor <samdoghor@gmail.com>   /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*    Created: 2023-07-18 02:53:21 by SamDoghor         \/        \/      \_/ *
+*    Updated: 2023-07-19 05:53:21 by SamDoghor                               *
+*                                                                            *
+******************************************************************************
+*/
+
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
