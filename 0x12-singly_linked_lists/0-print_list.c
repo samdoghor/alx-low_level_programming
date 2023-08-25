29d862e47635f203ba25bf3aa7b8bd89229ef972
samdoghor <56834362+samdoghor@users.noreply.github.com>
2023-08-25 14:59:25 -0700

    fix: bug-task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
-/****
+/**
  * print_list - This function prints out all the elements of a list_t list.
  * @h: singly linked list.
+ *
  * Return: total number of elements in the list.
  */
 
@@ -10,22 +11,22 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t x;
+	size_t x;
 
-    x = 0;
+	x = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-        h = h->next;
+		h = h->next;
 
-	x++;
-    }
+		x++;
+	}
 
-    return (x);
+	return (x);
 }
 
