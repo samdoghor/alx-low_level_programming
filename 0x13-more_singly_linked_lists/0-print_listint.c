9fc9772428329ed01be9693deabfe90b3f393ec3
samdoghor <56834362+samdoghor@users.noreply.github.com>
2023-08-29 11:51:45 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_int to print
  * Return: number of nodes in linked list
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t lst = 0;
@@ -16,7 +14,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		lst++;
 	}
-
 	return (lst);
 }
-
