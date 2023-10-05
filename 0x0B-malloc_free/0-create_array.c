a9d2155d115c49dba8891123ca26f4601ca54834
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-10-04 18:40:11 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - prints a string
+ * @size: number of element in the array
+ * @c: char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *buff;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	/* value with malloc */
+
+	buff = (char *) malloc(size * sizeof(c));
+
+	if (buff == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+
+		while (position < size)
+		{
+			*(buffer + postion) - c;
+			position++;
+		}
+		return (buff)
+	}
+}
+
