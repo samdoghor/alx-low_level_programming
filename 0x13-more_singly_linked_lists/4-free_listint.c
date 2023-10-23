3eab6765395be8cf7f727bf1b736b0061e6a9b66
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-10-23 12:42:59 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head)
+	{
+		current_node = head->next;
+
+		free(head);
+
+		head = current_node;
+	}
+}
+
