4fd01d8b3921368bd966c8a4e5ede4073c6aed64
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-05-03 02:09:24 +0100

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,29 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Return list len
- * @h: struct list
- * Return: list_len
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len = 0;
-	int i = 0;
+	int count;
 
-	for (i = 0; h; i++)
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		len += 1;
+		count++;
 		h = h->next;
 	}
-	return (len);
+
+	return (count);
 }
